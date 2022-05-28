585c4e46b822006801255744235e9b1900c1cf7f
12DerekH <55858151+12DerekH@users.noreply.github.com>
2022-05-28 18:14:21 -0400

    Main page
    Created the main page

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import '../App.css'
+import '../styles/Main.css'
+import {Link} from 'react-router-dom';
 
 const Main = () => {
     
     return (
-        <div>Main Page</div>
+        <div>
+            <a href="/"><img src="https://images.cooltext.com/5603665.png" width="291" height="95" alt="Quizzle" /></a>
+            <div class="container"> 
+                <h1>Access a quiz</h1><br/>
+                <Link class="login" exact to='/signup'>Dont have an account? Sign up here</Link>
+                <input class="info" type="text" placeholder="Quiz code"/>
+                <button>Start</button>
+            </div>
+        </div>
     )
 
 }
